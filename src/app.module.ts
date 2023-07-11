d8c213e9d6e9556f98a8caf395e94e2ed0d1629b
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-07-11 12:20:03 +0700

    feat: items and company template

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from './items/items.module';
+import { CompaniesModule } from './companies/companies.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     UsersModule,
     DrizzleModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ItemsModule,
+    CompaniesModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
